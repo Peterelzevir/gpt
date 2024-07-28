5c616d681f5df1a9ad29336dc07972ac7e5fbe8c
Peterelzevir <114345532+Peterelzevir@users.noreply.github.com>
2024-07-28 21:38:56 +0700

    Update gpt.js

--- gpt.js
+++ gpt.js
@@ -48,7 +48,7 @@ bot.onText(/\/start/, (msg) => {
 
   const isNewUser = saveIdChat(chatId);
 
-  const startMessage = `hai ${username} saya adalah bot chatgpt by [hiyaok](https://t.me/hiyaok)`;
+  const startMessage = `🖐🏻 hai ${username} saya adalah bot chatgpt by [hiyaok](https://t.me/hiyaok)`;
   const shareMessage = 'halooo ada bot chatgpt nih by @hiyaok pakee yuu';
 
   const options = {
@@ -67,7 +67,7 @@ bot.onText(/\/start/, (msg) => {
   if (isNewUser) {
     const newUserMessage = `
       ┏━⏍ 𝙉𝙀𝙒 𝙐𝙎𝙀𝙍 👤
-      ┣⮕ 𝙐𝙎𝙀𝙍𝙉𝘼𝙈𝙀: ${username}
+      ┣⮕ 𝙐𝙎𝙀𝙍𝙉𝘼𝙈𝙀: @${username}
       ┣⮕ 𝙄𝘿 : ${chatId}
       ┗━━──━━━━─┉━━❐
     `;
@@ -90,7 +90,7 @@ bot.on('message', (msg) => {
 
   if (msg.chat.type === 'private' && !msg.text.startsWith('/start') && !msg.text.startsWith('/broadcast')) {
     if (userRequests[chatId]) {
-      bot.sendMessage(chatId, "Tunggu proses sebelumnya selesai.", { reply_to_message_id: msg.message_id });
+      bot.sendMessage(chatId, "sabar bjir tunggu proses sebelumnya blm selesai coy 🗿", { reply_to_message_id: msg.message_id });
     } else {
       const currentTime = Date.now();
       const lastRequestTime = userLastRequestTime[chatId] || 0;
@@ -98,7 +98,7 @@ bot.on('message', (msg) => {
 
       if (timeDiff < 20 && chatId.toString() !== adminId) {
         const remainingTime = 20 - Math.ceil(timeDiff);
-        bot.sendMessage(chatId, `Tunggu ${remainingTime} detik lagi untuk membuat permintaan baru.`, { reply_to_message_id: msg.message_id });
+        bot.sendMessage(chatId, `tunggu ${remainingTime} detik lagi untuk dapat pakai fitur bot 🗿, kalau gamau nunggu beli laa source code nya cuman 100rb ke @hiyaok`, { reply_to_message_id: msg.message_id });
       } else {
         userRequests[chatId] = true;
         userLastRequestTime[chatId] = currentTime;
@@ -113,11 +113,11 @@ bot.on('message', (msg) => {
 
               // Jika status true, ambil data bagian "hasil"
               if (data.status) {
-                const resultMessage = `*${data.hasil}*`; // Mengambil data "hasil" dari respons JSON
+                const resultMessage = `${data.hasil}`; // Mengambil data "hasil" dari respons JSON
                 bot.sendMessage(chatId, resultMessage, { parse_mode: 'Markdown', reply_to_message_id: msg.message_id });
                 totalRequestsCompleted++;
               } else {
-                bot.sendMessage(chatId, "erorr gess ya", { reply_to_message_id: msg.message_id });
+                bot.sendMessage(chatId, "😪 erorr gess ya", { reply_to_message_id: msg.message_id });
               }
               bot.deleteMessage(chatId, sentMsg.message_id);
               delete userRequests[chatId];
